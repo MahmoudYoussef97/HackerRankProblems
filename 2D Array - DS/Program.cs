2491eeed686d1988db34a46d37c102e0b64185cb
MahmoudYoussef97 <Mahmoudyoussef0097@gmail.com>
2020-05-28 23:12:28 +0200

    Starting with array's problems

---
+++ 2D Array - DS/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the hourglassSum function below.
+    static int hourglassSum(int[][] arr)
+    {
+        List<int> values = new List<int>();
+        int hourglassSumValue = 0;
+        for(int i=0;i <4; ++i)
+        {
+            for(int j=0; j<4; ++j)
+            {
+                hourglassSumValue += arr[i + 1][j + 1]
+                                + arr[i][j] + arr[i][j + 1] + arr[i][j + 2]
+                                + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+                values.Add(hourglassSumValue);
+                hourglassSumValue = 0;
+            }
+        }
+        return values.Max();
+    }
+
+    static void Main(string[] args)
+    {
+
+        int[][] arr = new int[6][];
+
+        for (int i = 0; i < 6; i++)
+        {
+            arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+        }
+
+        int result = hourglassSum(arr);
+
+    }
+}
---
+++ New Year Chaos/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the minimumBribes function below.
+    static void minimumBribes(int[] q)
+    {
+        int minBribes = 0;
+        int min = q.Length;
+        for (int i = q.Length-1; i >= 0; --i)
+        {
+            if (q[i] - i > 3)
+            {
+                Console.WriteLine("Too chaotic");
+                return;
+            }
+            if (q[i] > i+1)
+                minBribes += q[i] - (i + 1);
+            else
+            {
+                if (min > q[i])
+                    min = q[i];
+                else if (min < q[i])
+                    minBribes++;
+            }          
+        }
+        Console.WriteLine(minBribes);
+        return;
+    }
+
+    static void Main(string[] args)
+    {
+        int t = Convert.ToInt32(Console.ReadLine());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string text = File.ReadAllText("C:\\Users\\mr_ho\\Downloads\\test.txt");
+
+            int[] q = Array.ConvertAll(text.Split(' '), qTemp => Convert.ToInt32(qTemp))
+            ;
+            minimumBribes(q);
+        }
+    }
+}
