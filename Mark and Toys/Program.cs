98bbd9ea08f2fed8f7fea30427e0463eb701c879
MahmoudYoussef97 <Mahmoudyoussef0097@gmail.com>
2020-06-07 01:27:18 +0200

    Starting sorting problems and solving 2 problems

---
+++ Mark and Toys/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the maximumToys function below.
+    static int maximumToys(int[] prices, int k)
+    {
+        long currentTotalPrice = 0;
+        int maximumNumberOfToys = 0;
+        Array.Sort(prices);
+        foreach (var price in prices)
+        {
+            currentTotalPrice += price;
+            if (currentTotalPrice > k)
+                break;
+            else
+                maximumNumberOfToys++;
+        }
+        return maximumNumberOfToys;
+    }
+
+    static void Main(string[] args)
+    {
+        string[] nk = Console.ReadLine().Split(' ');
+
+        int n = Convert.ToInt32(nk[0]);
+
+        int k = Convert.ToInt32(nk[1]);
+
+        int[] prices = Array.ConvertAll(Console.ReadLine().Split(' '), pricesTemp => Convert.ToInt32(pricesTemp))
+        ;
+        int result = maximumToys(prices, k);
+    }
+}
---
+++ Sorting Bubble Sort/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the countSwaps function below.
+    static void countSwaps(int[] a)
+    {
+        int numberOfSwaps = 0;
+        for(int i=0;i<a.Length;++i)
+        {
+            for(int j=0;j<a.Length-1;++j)
+            {
+                if(a[j] > a[j+1])
+                {
+                    Swap(a, j, j+1);
+                    numberOfSwaps++;
+                }
+            }
+        }
+        Console.WriteLine($"Array is sorted in {numberOfSwaps} swaps.");
+        Console.WriteLine($"First Element: {a[0]}");
+        Console.WriteLine($"Last Element: {a[a.Length-1]}");
+    }
+    static void Swap(int[]a, int firstIndex, int secondIndex)
+    {
+        int temp = a[firstIndex];
+        a[firstIndex] = a[secondIndex];
+        a[secondIndex] = temp;
+    }
+
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), aTemp => Convert.ToInt32(aTemp))
+        ;
+        countSwaps(a);
+    }
+}
